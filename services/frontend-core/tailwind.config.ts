8003721298a982774df566ef9ea0d88a42af6a49
Adele Gauvrit <gauvrit.adele@gmail.com>
2023-08-17 17:31:47 +0200

    feat(frontend-core): add fonts

--- services/frontend-core/tailwind.config.ts
+++ services/frontend-core/tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      title: ['Caprasimo', 'ui-serif'],
+      body: ['baskerville', 'ui-serif'],
+    },
   },
   plugins: [],
 };
